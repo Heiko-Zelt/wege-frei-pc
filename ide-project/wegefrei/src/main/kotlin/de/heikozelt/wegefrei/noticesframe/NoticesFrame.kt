9e667eb54f84fa2f1abea4d290b684ce377dfd55
Heiko Zelt <hz@heikozelt.de>
2022-10-29 18:05:32 +0200

    replace BorderLayout and ButtonBar in NoticesFrame with GroupLayout. add ScrollPane

--- ide-project/wegefrei/src/main/kotlin/de/heikozelt/wegefrei/noticesframe/NoticesFrame.kt
+++ ide-project/wegefrei/src/main/kotlin/de/heikozelt/wegefrei/noticesframe/NoticesFrame.kt
@@ -2,14 +2,10 @@ package de.heikozelt.wegefrei.noticesframe
 
 import de.heikozelt.wegefrei.WegeFrei
 import de.heikozelt.wegefrei.entities.Notice
-import de.heikozelt.wegefrei.gui.Styles
 import de.heikozelt.wegefrei.jobs.LoadNoticesWorker
 import de.heikozelt.wegefrei.model.NoticesTableModel
 import org.slf4j.LoggerFactory
-import java.awt.BorderLayout
-import javax.swing.JFrame
-import javax.swing.JPanel
-import javax.swing.JTable
+import javax.swing.*
 
 /**
  * Der NoticesFrame zeigt eine Übersichts-Tabelle mit allen Meldungen an.
@@ -26,35 +22,84 @@ import javax.swing.JTable
 class NoticesFrame(private val app: WegeFrei) : JFrame("Meldungen - Wege frei!") {
 
     private val log = LoggerFactory.getLogger(this::class.java.canonicalName)
-    private val scrollPanel = JPanel(BorderLayout())
     private val noticesTableModel = NoticesTableModel()
-    private val noticesTable = JTable(noticesTableModel)
-    private val noticesToolBar = NoticesButtonsBar(app)
+
 
     init {
         log.debug("init")
-        layout = BorderLayout(5,5)
-        setSize(1000, 700)
-        defaultCloseOperation = EXIT_ON_CLOSE
-        background = Styles.FRAME_BACKGROUND
 
+        // GUI components
+        val noticesTable = JTable(noticesTableModel)
         noticesTable.addMouseListener(NoticesTableMouseListener(app))
         noticesTable.getColumn("Farbe").cellRenderer = NoticesTableColorCellRenderer()
         noticesTable.getColumn("Status").cellRenderer = NoticesTableStateCellRenderer()
+        val scrollPane = JScrollPane(noticesTable)
+        val newButton = JButton("neue Meldung erfassen")
+        newButton.addActionListener { app.openNoticeFrame() }
+        val scanButton = JButton("Scan")
+        scanButton.addActionListener { app.scanForNewPhotos() }
+        val settingsButton = JButton("Einstellungen")
+        settingsButton.addActionListener{ app.openSettingsFrame() }
+        val helpButton = JButton("Hilfe")
+        helpButton.addActionListener { log.debug("unhandled event") }
+
+        // layout:
+        val lay = GroupLayout(contentPane)
+        lay.autoCreateGaps = true
+        lay.autoCreateContainerGaps = true
+        // left to right
+        lay.setHorizontalGroup(
+            lay.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addComponent(scrollPane)
+                .addGroup(
+                    lay.createSequentialGroup()
+                        .addPreferredGap(
+                            LayoutStyle.ComponentPlacement.RELATED,
+                            GroupLayout.PREFERRED_SIZE,
+                            Int.MAX_VALUE
+                        )
+                        .addComponent(newButton)
+                        .addComponent(scanButton)
+                        .addComponent(settingsButton)
+                        .addComponent(helpButton)
+                )
+        )
+        // top to bottom
+        lay.setVerticalGroup(
+            lay.createSequentialGroup()
+                .addComponent(scrollPane)
+                .addGroup(
+                    lay.createParallelGroup()
+                        .addComponent(newButton)
+                        .addComponent(scanButton)
+                        .addComponent(settingsButton)
+                        .addComponent(helpButton)
+                )
+        )
+        layout = lay
+
+        /*
+        layout = BorderLayout(5,5)
+        background = Styles.FRAME_BACKGROUND
         noticesTable.background = Styles.NOTICES_TABLE_BACKGROUND
         noticesTable.foreground = Styles.TEXT_COLOR
         scrollPanel.add(noticesTable.tableHeader, BorderLayout.NORTH)
         scrollPanel.add(noticesTable, BorderLayout.CENTER)
-
         add(scrollPanel)
         add(noticesToolBar, BorderLayout.SOUTH)
+        */
 
+        setSize(1000, 700)
+        defaultCloseOperation = EXIT_ON_CLOSE
         isVisible = true
     }
 
     fun loadData() {
-        // todo Fehlermeldung ausgeben
-        val dbRepo = app.getDatabaseRepo()?:return
+        val dbRepo = app.getDatabaseRepo()
+        if(dbRepo == null) {
+            log.error("databaseRepo ist null")
+            return
+        }
         val worker = LoadNoticesWorker(dbRepo, noticesTableModel)
         worker.execute()
     }
@@ -73,5 +118,4 @@ class NoticesFrame(private val app: WegeFrei) : JFrame("Meldungen - Wege frei!")
     fun noticeDeleted(notice: Notice) {
         noticesTableModel.removeNotice(notice)
     }
-
 }
