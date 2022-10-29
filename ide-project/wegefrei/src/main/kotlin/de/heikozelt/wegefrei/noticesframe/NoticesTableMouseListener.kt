dbc89bfc1e4d9ddc9d88fbf5e9ebe9c99aeaf1e6
Heiko Zelt <hz@heikozelt.de>
2022-10-29 18:27:28 +0200

    check type of event source and table model with if(...is...) instead of simple type casts

--- ide-project/wegefrei/src/main/kotlin/de/heikozelt/wegefrei/noticesframe/NoticesTableMouseListener.kt
+++ ide-project/wegefrei/src/main/kotlin/de/heikozelt/wegefrei/noticesframe/NoticesTableMouseListener.kt
@@ -2,6 +2,7 @@ package de.heikozelt.wegefrei.noticesframe
 
 import de.heikozelt.wegefrei.WegeFrei
 import de.heikozelt.wegefrei.model.NoticesTableModel
+import org.slf4j.LoggerFactory
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.JTable
@@ -10,17 +11,22 @@ import javax.swing.JTable
  * Ein Doppelklick auf eine Meldung in der Tabelle öffnet ein Fenster zum Bearbeiten der Meldung.
  */
 class NoticesTableMouseListener(private val app: WegeFrei): MouseAdapter() {
+    private val log = LoggerFactory.getLogger(this::class.java.canonicalName)
     override fun mouseClicked(me: MouseEvent) {
         if (me.clickCount == 2) { // double click
-            val table = me.source as JTable
-            val rowIndex = table.selectedRow
-            //val column = table.selectedColumn
-            //val id = table.getValueAt(rowIndex, 0) as Int
-            //JOptionPane.showMessageDialog(null, "$row, $column, $id")
-            val model = table.model as NoticesTableModel
-            // open new Window to edit existing notice
-            val notice = model.getNoticeAt(rowIndex)
-            app.openNoticeFrame(notice)
+            val table = me.source
+            if(table is JTable) {
+                val rowIndex = table.selectedRow
+                val model = table.model
+                if(model is NoticesTableModel) {
+                    val notice = model.getNoticeAt(rowIndex)
+                    app.openNoticeFrame(notice)
+                } else {
+                    log.error("Model ist keine NoticesTableModel")
+                }
+            } else {
+                log.error("Quelle ist keine JTable")
+            }
         }
     }
 }
